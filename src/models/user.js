0b78d16c5db80d7d49cd07db27678d9f1fa9ccdd
akshitakanojia <akshitakanojia786@gmail.com>
2021-10-09 22:53:17 +0530

    backend complete

---
+++ src/models/user.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Invalid email')
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true
+    }
+  }]
+}, {
+  timestamps: true
+})
+
+userSchema.methods.toJSON = function () {
+  const user = this
+  const userObject = user.toObject()
+
+  delete userObject.password
+  delete userObject.tokens
+
+  return userObject
+}
+
+//Hash the plain text password before saving
+userSchema.pre('save', async function (next) {
+  const user = this
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+  next()
+})
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
+  return token
+}
+
+//static methods are accessible on the model also called model methods
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    throw new Error('Unable to login')
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch) {
+    throw new Error('Unable to login')
+  }
+
+  return user
+}
+
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
---
+++ src/routes/user.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+
+const router = new express.Router()
+
+router.post('/users', async (req, res) => {
+  const user = new User(req.body)
+  console.log(req.body);
+  try {
+    await user.save()
+    const token = await user.generateAuthToken()
+    res.status(201).send({ user, token })
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error)
+  }
+})
+
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    if (!user) {
+      res.status(401).send();
+    }
+    const token = await user.generateAuthToken()
+    res.send({ user, token })
+  } catch (error) {
+    if (error.message === "Unable to login") {
+      return res.status(401).send({ message: error.message })
+    }
+    res.status(400).send(error)
+  }
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token
+    })
+    await req.user.save()
+    res.send()
+  } catch (error) {
+    res.status(500).send()
+  }
+})
+
+module.exports = router
